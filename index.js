09c35c68395674450658b64a5778bfc388efbe0e
Vinccent Caraccio <vincent.caraccio@nibbio.io>
2022-08-09 11:25:10 +0800

    completed computation

--- index.js
+++ index.js
@@ -8,14 +8,35 @@ const github = require('@actions/github');
     const token = core.getInput('token');
     const owner = core.getInput('owner');
     const repo = core.getInput('repo');
+    const major = core.getInput('major');
+    const minor = core.getInput('minor');
+    const patch = core.getInput('patch');
     const octokit = github.getOctokit(token);
-    const res = await octokit.request(
+    const { data } = await octokit.request(
       'GET /repos/{owner}/{repo}/actions/artifacts',
       { owner, repo }
     );
-    console.log(`res: ${JSON.stringify(res, null, 2)}`);
-
-    core.exportVariable('RELEASE_VERSION', '0.0.0');
+    const sortedVersions = data.artifacts
+      .map(a => (a.name.match(/\d+\.\d+\.\d+/) || [])[0])
+      .filter(v => !!v)
+      .map(v => v.split('.').map(t => parseInt(t)))
+      .sort((a, b) =>
+        a[0] === b[0] ?
+          a[1] === b[1] ?
+            a[2] - b[2] :
+            a[1] - b[1] :
+          a[0] - b[0]);
+    if (sortedVersions.length > 0) {
+      const lastVersion = sortedVersions[sortedVersions.length - 1];
+      const nextVersion = [
+        major || lastVersion[0],
+        minor || lastVersion[1],
+        patch || lastVersion[2] + 1,
+      ];
+      core.exportVariable('RELEASE_VERSION', nextVersion.join('.'));
+    } else {
+      core.exportVariable('RELEASE_VERSION', '0.0.0');
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
