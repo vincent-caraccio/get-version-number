40d7f21b1700921c7e5ba7fe6dc066dc6bb64346
Vinccent Caraccio <vincent.caraccio@nibbio.io>
2022-08-09 10:53:27 +0800

    bugfix

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const token = core.getInput('token');
-  const owner = core.getInput('owner');
-  const repo = core.getInput('repo');
-  const octokit = github.getOctokit(token);
-  const res = await octokit.request(
-    'GET /repos/{owner}/{repo}/actions/artifacts',
-    { owner, repo }
-  );
-  console.log(`res: ${JSON.stringify(res, null, 2)}`);
+(async function () {
+  try {
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const octokit = github.getOctokit(token);
+    const res = await octokit.request(
+      'GET /repos/{owner}/{repo}/actions/artifacts',
+      { owner, repo }
+    );
+    console.log(`res: ${JSON.stringify(res, null, 2)}`);
 
-  core.exportVariable('RELEASE_VERSION', '0.0.0');
-} catch (error) {
-  core.setFailed(error.message);
-}
+    core.exportVariable('RELEASE_VERSION', '0.0.0');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})().catch(error => core.setFailed(error.message));
