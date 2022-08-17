75a7e88ad793b0ca17923da6c7aae01cf5f2697c
Vincent Caraccio <vincent.caraccio@nibbio.io>
2022-08-17 20:13:10 +0100

    bugfix default version

--- index.js
+++ index.js
@@ -4,30 +4,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 (async function () {
+  const major = core.getInput('major');
+  const minor = core.getInput('minor');
+  const patch = core.getInput('patch');
+  const nextVersion = [major || 0, minor || 0, patch || 1];
+
   try {
     const token = core.getInput('token');
     const repository = core.getInput('repository');
     const [owner, repo] = repository.split('/');
-    const major = core.getInput('major');
-    const minor = core.getInput('minor');
-    const patch = core.getInput('patch');
     const octokit = github.getOctokit(token);
     const { data } = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', { owner, repo });
     const tag = data ? data.tag_name : '';
     if (tag && tag.match(/\d+\.\d+\.\d+/)) {
       const lastVersion = tag.split('.').map(t => parseInt(t));
-      const nextVersion = [
-        major || lastVersion[0],
-        minor || lastVersion[1],
-        patch || lastVersion[2] + 1,
-      ];
-      core.exportVariable('RELEASE_VERSION', nextVersion.join('.'));
-    } else {
-      core.exportVariable('RELEASE_VERSION', '0.0.1');
+      nextVersion[0] = major || lastVersion[0];
+      nextVersion[1] = major || lastVersion[1];
+      nextVersion[2] = major || lastVersion[2];
     }
+    core.exportVariable('RELEASE_VERSION', nextVersion.join('.'));
   } catch (error) {
     if (error.message === 'Not Found') {
-      core.exportVariable('RELEASE_VERSION', '0.0.1');
+      core.exportVariable('RELEASE_VERSION', nextVersion.join('.'));
     } else {
       core.setFailed(error.message);
     }
